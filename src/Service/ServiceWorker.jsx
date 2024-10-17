94b79b9f8bf9c6e89501e0d671f8a62a1875d6b0
Cristian Ramirez <ccrhdbj@outlook.com>
2024-10-17 12:58:40 -0500

    service worker

--- src/Service/ServiceWorker.jsx
+++ src/Service/ServiceWorker.jsx
@@ -1,4 +1,18 @@
 const ServiceWorker = () => {
-
+  if ("serviceWorker" in navigator) {
+    window.addEventListener("load", function () {
+      navigator.serviceWorker.register("../sw.js").then(
+        function (registration) {
+          console.log(
+            "ServiceWorker registration successful with scope: ",
+            registration.scope
+          );
+        },
+        function (err) {
+          console.log("ServiceWorker registration failed: ", err);
+        }
+      );
+    });
+  }
 }
 export { ServiceWorker }
