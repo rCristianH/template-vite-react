2032653da83c33df0e5b84a4e87dbacf3cf81164
Cristian Ramirez <ccrhdbj@outlook.com>
2024-11-28 17:14:36 -0500

    initial and install xstate

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { ServiceWorker } from './Service/ServiceWorker'
+import { App } from './App'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    Hello
+    <App />
+    <ServiceWorker />
   </StrictMode>,
 )
